3a5290f28dc2ec276a9ca824e39ed85e91b30611
Francisca105 <franciscaalmeida023@gmail.com>
2023-12-05 00:12:47 +0000

    Entrega do relatório + script para auxiliar testes

--- gerador/testes.sh
+++ gerador/testes.sh
@@ -8,8 +8,8 @@ for ((X = $inicio; X <= $fim; X += $incremento)); do
     ./gen $X $X 100 > ./t$X.in
 done
 
-for ((X = $inicio; X <= $fim; X += $incremento)); do
-    echo -e "Teste $X\n" >> output.txt
-    { time -p ../proj < ./t$X.in; } 2>&1 | grep real >> output.txt
-    echo -e "\n" >> output.txt
-done
+# for ((X = $inicio; X <= $fim; X += $incremento)); do
+#     echo -e "Teste $X\n" >> output.txt
+#     { time -p ../proj < ./t$X.in; } 2>&1 | grep real >> output.txt
+#     echo -e "\n" >> output.txt
+# done
