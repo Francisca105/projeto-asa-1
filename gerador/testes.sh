f8564ba8d41849b2414da5ac30b03743004ab6e0
Francisca105 <franciscaalmeida023@gmail.com>
2023-12-05 00:47:26 +0000

    melhorado

--- gerador/testes.sh
+++ gerador/testes.sh
@@ -2,14 +2,14 @@
 
 incremento=250
 inicio=0
-fim=5000
+fim=1000
 
 for ((X = $inicio; X <= $fim; X += $incremento)); do
-    ./gen $X $X 100 > ./t$X.in
+    ./gen $X $X 100 > ./testes/in/t$X.in
 done
 
-# for ((X = $inicio; X <= $fim; X += $incremento)); do
-#     echo -e "Teste $X\n" >> output.txt
-#     { time -p ../proj < ./t$X.in; } 2>&1 | grep real >> output.txt
-#     echo -e "\n" >> output.txt
-# done
+for ((X = $inicio; X <= $fim; X += $incremento)); do
+    hyperfine "../proj < ./testes/in/t$X.in" --export-json "./testes/out/t$X.json"
+done
+
+wait
